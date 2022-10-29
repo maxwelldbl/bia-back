cb82da34115ad24e4f7c338f944677ed43e31538
jdrb2098 <juan.rondon.bonilla@gmail.com>
2022-10-29 08:42:23 -0500

    email config

--- backend/settings.py
+++ backend/settings.py
@@ -209,7 +209,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.outlook.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER='bia-bia-cormacarena@outlook.com'
+EMAIL_HOST_USER='bia-cormacarena@outlook.com'
 EMAIL_HOST_PASSWORD='Isamc1234'
 
 # Send SMS with Twilio
